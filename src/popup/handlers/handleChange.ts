152c07d3ba880439079c3000a0aa9412fa2d4e93
Maxylan <maxylan@github.com>
2024-05-24 23:45:45 +0200

    Some change to handleChange, its 23:45 and Im tiered

--- src/popup/handlers/handleChange.ts
+++ src/popup/handlers/handleChange.ts
@@ -24,7 +24,7 @@ const onIncomeChange = async (e: any, income_id: number) => {
         }
         
         // const res = await browser.storage.local.set({ incomes: storage.incomes });
-        const res = await browser.storage.local.set(storage);
+        await browser.storage.local.set(storage);
     }
 }
 
